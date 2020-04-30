699bcb252fd3ac7ec282bf093b0840f196073f6c
Kota Miura <kotapub@gmail.com>
2020-04-30 16:52:06 +0200

    JSON_read.js replaced

--- JSON_Read.js
+++ JSON_Read.js
@@ -9,9 +9,14 @@ function readJSON(call) {
 
     //Path to the WFE_input_params.json
     var jsonDir = "/params/WFE_input_params.json";
-    var settings = JSON.parse(IJ.openAsString(jsonDir));
-    // var allPropertyNames = Object.keys(settings);	// put JSON keys in new Array
-    // var count = Object.keys(settings).length;		// count number of JSON keys
+    var f = new java.io.File( jsonDir );
+    if ( f.exists() ){
+        var settings = JSON.parse(IJ.openAsString(jsonDir));
+        // var allPropertyNames = Object.keys(settings);	// put JSON keys in new Array
+        // var count = Object.keys(settings).length;		// count number of JSON keys
+    } else {
+        var settings = JSON.parse( java.lang.System.getenv()['WFE_INPUT_JSON'] );
+    }
     return (eval(call));
 }
 
